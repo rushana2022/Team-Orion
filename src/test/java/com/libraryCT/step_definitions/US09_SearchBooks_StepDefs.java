8ae02f14107db05c5b5468ecdf403c8f3eb2c122
ali_kis <alikis25@gmail.com>
2023-03-08 12:08:50 -0500

    US09 Search Book feature created

--- src/test/java/com/libraryCT/step_definitions/US09_SearchBooks_StepDefs.java
+++ src/test/java/com/libraryCT/step_definitions/US09_SearchBooks_StepDefs.java
@@ -23,7 +23,7 @@ public class US09_SearchBooks_StepDefs {
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        Driver.getDriver().get(ConfigurationReader.getProperty("libraryUrl"));
     }
     @Given("I login to application as a student")
     public void i_login_to_application_as_a_student() {
