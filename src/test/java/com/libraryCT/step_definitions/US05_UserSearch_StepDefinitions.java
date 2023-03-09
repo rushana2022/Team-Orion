46479de5d5b102d3e8df4bd540f9de5ef1318deb
Tulay <tulay0109@hotmail.com>
2023-03-09 12:57:12 -0500

    US05 User search values page has been completed

---
+++ src/test/java/com/libraryCT/step_definitions/US05_UserSearch_StepDefinitions.java
@@ -0,0 +1,41 @@
+package com.libraryCT.step_definitions;
+
+import com.libraryCT.pages.LoginPage_RB;
+import com.libraryCT.pages.SearchPage;
+import com.libraryCT.utilities.BrowserUtils;
+import com.libraryCT.utilities.ConfigurationReader;
+import com.libraryCT.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class US05_UserSearch_StepDefinitions {
+
+
+    SearchPage searchPage = new SearchPage();
+    LoginPage_RB loginPage_rb = new LoginPage_RB();
+
+
+      @Then("table should contain rows with {string}")
+    public void table_should_contain_rows_with_tests(String test) {
+        List<String> allEmails = BrowserUtils.getElementsText(searchPage.allEmails);
+        for (String allEmail : allEmails) {
+            Assert.assertTrue(allEmail.contains(test));
+        }
+    }
+
+    @Then("table should have following column names:")
+    public void table_should_have_following_column_names(List<String> dataTable) {
+
+        List<String> dataInfo = BrowserUtils.getElementsText(searchPage.columns);
+        //Looping through the List<WebElement>, getting all options' texts, and storing them into List<String>
+        Assert.assertTrue(dataInfo.containsAll(dataTable));
+
+        }
+    }
+
